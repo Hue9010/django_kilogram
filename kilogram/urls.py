7fc643e61702c2ad360a0d03fc58f7a0733fe27f
hue9010 <hue9010@gmail.com>
2017-11-19 20:50:48 +0900

    소셜로그인 구현, URL 리디렉션 문제해결 남음

--- kilogram/urls.py
+++ kilogram/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.decorators import login_required
 
 app_name ='kilogram'
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name = 'index'),
+  url(r'^$', login_required(views.IndexView.as_view()), name = 'index'),
     url(r'^upload$', views.upload, name = 'upload')
 ]
