65edd4d55cdb947fa6b327ad9cf97388085282ec
hue9010 <hue9010@gmail.com>
2017-12-02 17:58:31 +0900

    프로파일 페이지 만들기 & 업데이트

--- kilogram/views.py
+++ kilogram/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
-# from django.contrib.auth.forms import UserCreationForm
+from django.views import View
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-from .forms import CreateUserForm, UploadForm
+from .models import Profile
+from .forms import UploadForm, UserForm, ProfileForm
 # Create your views here.
 
 @login_required
@@ -38,3 +38,46 @@ class ProfileView(DetailView):
     context_object_name = 'profile_user'
     model = User
     template_name = 'kilogram/profile.html'
+
+
+class ProfileUpdateView(View):
+    def get(self, request):
+        user = get_object_or_404(User, pk=request.user.pk)
+
+        user_form = UserForm(initial={
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+        })
+
+        if hasattr(user, 'profile'):
+            profile = user.profile
+            profile_form = ProfileForm(initial={
+                'nickname': user.profile.nickname,
+                'profile_photo': profile.profile_photo,
+            })
+        else:
+            profile_form = ProfileForm()
+
+        return render(request, 'kilogram/profile_update.html',
+                      {"user_form": user_form, "profile_form": profile_form})
+
+    def post(self, request):
+        pk = request.user.pk
+        u = User.objects.get(id=pk)
+        user_form = UserForm(request.POST, instance=u)
+
+        if user_form.is_valid():
+            user_form.save()
+
+        if hasattr(u, 'profile'):
+            profile = u.profile
+            profile_form = ProfileForm(request.POST, request.FILES, instance=profile)
+        else:
+            profile_form = ProfileForm(request.POST, request.FILES)
+
+        if profile_form.is_valid():
+            profile = profile_form.save(commit=False)
+            profile.user = u
+            profile.save()
+
+        return redirect('kilogram:profile', pk)
